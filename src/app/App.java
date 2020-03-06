4feb5326d967df5905c74d6f0b94bdc007c593f9
Carlos Becerra01 <geminiscarlos.12@gmail.com>
2020-03-06 10:51:30 -0500

    Editing client file and run app

---
+++ src/app/App.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app;
+
+import vista.JFLogin;
+
+public class App {
+    
+    public static void main(String[] args) 
+    {
+        JFLogin fLogin = new JFLogin();
+        fLogin.setVisible(true);
+    }
+    
+}
--- src/modelo/arreglo_Cliente.java
+++ src/modelo/arreglo_Cliente.java
@@ -1,5 +1,5 @@
-
 package modelo;
+
 import bean.Cliente;
 import java.io.BufferedReader;
 import java.io.File;
@@ -11,109 +11,115 @@ import java.util.StringTokenizer;
 import javax.swing.JOptionPane;
 
 public class arreglo_Cliente {
+
     private ArrayList<Cliente> lista;
 
     public arreglo_Cliente() {
         lista = new ArrayList<Cliente>();
         cargar();
     }
-    public String adicionar(Cliente p){
+
+    public String adicionar(Cliente p) {
         String mensaje;
         Cliente x = buscar(p.getCodigo());
-        if (x==null){
+        if (x == null) {
             lista.add(p);
             mensaje = "El cliente se guardo correctamente";
-        }else{
+        } else {
             mensaje = "El cliente ya existe";
         }
         return mensaje;
-    }   
-    public void eliminar(Cliente p){
+    }
+
+    public void eliminar(Cliente p) {
         lista.remove(p);
     }
-    
-    public Cliente obtener(int posicion){
+
+    public Cliente obtener(int posicion) {
         return lista.get(posicion);
-    }  
-    public Cliente buscar(int codigo){
-        Cliente p=null;
-        for(int i=0; i<lista.size(); i++){
-            if(obtener(i).getCodigo()==codigo){
+    }
+
+    public Cliente buscar(int codigo) {
+        Cliente p = null;
+        for (int i = 0; i < lista.size(); i++) {
+            if (obtener(i).getCodigo() == codigo) {
                 p = obtener(i);
                 i = lista.size();
             }
         }
         return p;
-    }    
-    public int buscarPosicion(int codigo){
+    }
+
+    public int buscarPosicion(int codigo) {
         int posicion = -1;
-        for(int i=0; i<lista.size(); i++){
-            if(obtener(i).getCodigo()==codigo){
+        for (int i = 0; i < lista.size(); i++) {
+            if (obtener(i).getCodigo() == codigo) {
                 posicion = i;
                 break;
             }
         }
         return posicion;
     }
-    public void actualizar(Cliente p){
+
+    public void actualizar(Cliente p) {
         lista.set(buscarPosicion(p.getCodigo()), p);
     }
-    
-    
-    public int totalCliente(){
+
+    public int totalCliente() {
         return lista.size();
-    }  
-    private void cargar(){
-        try{
-            File archivo = new File("cliente.txt");
-            if(archivo.exists()){
-                BufferedReader br=new BufferedReader(new FileReader("cliente.txt"));
+    }
+
+    private void cargar() {
+        try {
+            File archivo = new File("./archivos/cliente.txt");
+            if (archivo.exists()) {
+                BufferedReader br = new BufferedReader(new FileReader("./archivos/cliente.txt"));
                 String linea;
 
-                while((linea=br.readLine())!=null){
-                    StringTokenizer st=new StringTokenizer(linea,",");
+                while ((linea = br.readLine()) != null) {
+                    StringTokenizer st = new StringTokenizer(linea, ",");
 
                     int codigo = Integer.parseInt(st.nextToken().trim());
-                    String nombre=st.nextToken().trim();
-                    String apePaterno=st.nextToken().trim();
-                    String apeMaterno=st.nextToken().trim();
+                    String nombre = st.nextToken().trim();
+                    String apePaterno = st.nextToken().trim();
+                    String apeMaterno = st.nextToken().trim();
                     int edad = Integer.parseInt(st.nextToken().trim());
-                    int dni = Integer.parseInt(st.nextToken().trim());                    
+                    int dni = Integer.parseInt(st.nextToken().trim());
                     int telefono = Integer.parseInt(st.nextToken().trim());
-                    String direccion=st.nextToken().trim();
-                    String licencia=st.nextToken().trim();
-                    
-                  
+                    String direccion = st.nextToken().trim();
+                    String licencia = st.nextToken().trim();
 
-                    Cliente x =new Cliente(codigo, nombre, apePaterno, apeMaterno, edad, dni,                        
+                    Cliente x = new Cliente(codigo, nombre, apePaterno, apeMaterno, edad, dni,
                             telefono, direccion, licencia);
                     adicionar(x);
                 }
                 br.close();
+            } else {
+                JOptionPane.showMessageDialog(null, "Abriendo el programa");
             }
-            else
-                JOptionPane.showMessageDialog(null,"Abriendo el programa");
-            } catch(Exception x){
-                JOptionPane.showMessageDialog(null,"Se produjo un error: "+x.toString());
-            }
+        } catch (Exception x) {
+            JOptionPane.showMessageDialog(null, "Se produjo un error: " + x.toString());
+        }
 
     }
-    public void grabar(){
-        try{
-            PrintWriter pw =new PrintWriter(new FileWriter("Cliente.txt"));
-            for(int i=0; i<totalCliente(); i++)
-                pw.println(obtener(i).getCodigo()+","+
-                        obtener(i).getNombre()+","+
-                        obtener(i).getApePaterno()+","+
-                        obtener(i).getApeMaterno()+","+
-                        obtener(i).getEdad()+","+
-                        obtener(i).getDni()+","+
-                        obtener(i).getTelefono()+","+
-                        obtener(i).getDireccion()+","+
-                        obtener(i).getLicencia());
+
+    public void grabar() {
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter("./archivos/cliente.txt"));
+            for (int i = 0; i < totalCliente(); i++) {
+                pw.println(obtener(i).getCodigo() + ","
+                        + obtener(i).getNombre() + ","
+                        + obtener(i).getApePaterno() + ","
+                        + obtener(i).getApeMaterno() + ","
+                        + obtener(i).getEdad() + ","
+                        + obtener(i).getDni() + ","
+                        + obtener(i).getTelefono() + ","
+                        + obtener(i).getDireccion() + ","
+                        + obtener(i).getLicencia());
+            }
             pw.close();
-        }catch(Exception x){
-            JOptionPane.showMessageDialog(null,"Se produjo un error: "+x.toString());
+        } catch (Exception x) {
+            JOptionPane.showMessageDialog(null, "Se produjo un error: " + x.toString());
         }
-    }    
+    }
 }
